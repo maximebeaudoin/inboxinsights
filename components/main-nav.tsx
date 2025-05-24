187572a163f5732f46a2451e3b8e56caed8edbe5
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2025-05-24 14:26:46 -0400

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -7,10 +7,7 @@ export default function MainNav() {
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
       <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
         <div className="flex gap-5 items-center font-semibold">
-          <Link href={'/'}>InboxInsights</Link>
-          <Link href={'/dashboard/mood-meter'} className="text-sm font-normal hover:underline">
-            Mood Meter
-          </Link>
+          <Link href={'/dashboard/mood-meter'}>InboxInsights - Mood Meter</Link>
         </div>
         <HeaderAuth />
       </div>
