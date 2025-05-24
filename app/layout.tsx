e0deaced118cff519fda48c47976c98202da38ca
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2025-05-24 11:48:36 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+            <div className="flex-1 w-full flex flex-col gap-5 items-center">
               <MainNav />
               <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
 
